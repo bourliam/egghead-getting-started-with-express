6d9c701fcbd8c7ec6c424ad7d149f91f52387c58
Maxime Bourliatoux <bourliam@gmail.com>
2018-06-10 17:35:33 -0700

    01

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello world')
+  res.send('Hello Maxou !')
 })
 
 app.get('/yo', function (req, res) {
-  res.send('YO!')
+  res.send('yo Max!')
 })
 
 var server = app.listen(3000, function () {
-  console.log('Server running at http://localhost:' + server.address().port)
+  console.log('Server running!! \n => http://localhost:' + server.address().port)
 })
